8ef0bd34f70f59b173335038cf7b4d4688d79174
Josh Benton <Josh.Benton.CE@gmail.com>
2016-07-06 11:59:01 -0400

    Persistant storage

--- EDI-Utilities/Properties/Settings.Designer.cs
+++ EDI-Utilities/Properties/Settings.Designer.cs
@@ -8,33 +8,150 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace EDI_Utilities.Properties
-{
-
-
+namespace EDI_Utilities.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        internal string conversionDelimiter;
-        internal int conversionDescriptionCol;
-        internal bool conversionDisjoint;
-        internal bool conversionHoldSegValue;
-        internal int conversionIdocFieldCol;
-        internal int conversionIdocSegmentCol;
-        internal bool conversionIntenseSearch;
-        internal int conversionSkipFirstX;
-        internal int conversionX12Col;
-        internal bool explorerSmartMode;
-        internal bool fallbackSearchEnabled;
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string conversionDelimiter {
+            get {
+                return ((string)(this["conversionDelimiter"]));
+            }
+            set {
+                this["conversionDelimiter"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public int conversionIdocFieldCol {
+            get {
+                return ((int)(this["conversionIdocFieldCol"]));
+            }
+            set {
+                this["conversionIdocFieldCol"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public int conversionIdocSegmentCol {
+            get {
+                return ((int)(this["conversionIdocSegmentCol"]));
+            }
+            set {
+                this["conversionIdocSegmentCol"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public int conversionX12Col {
+            get {
+                return ((int)(this["conversionX12Col"]));
+            }
+            set {
+                this["conversionX12Col"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public int conversionSkipFirstX {
+            get {
+                return ((int)(this["conversionSkipFirstX"]));
+            }
+            set {
+                this["conversionSkipFirstX"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public int conversionDescriptionCol {
+            get {
+                return ((int)(this["conversionDescriptionCol"]));
+            }
+            set {
+                this["conversionDescriptionCol"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool conversionHoldSegValue {
+            get {
+                return ((bool)(this["conversionHoldSegValue"]));
+            }
+            set {
+                this["conversionHoldSegValue"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool conversionIntenseSearch {
+            get {
+                return ((bool)(this["conversionIntenseSearch"]));
+            }
+            set {
+                this["conversionIntenseSearch"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool explorerSmartMode {
+            get {
+                return ((bool)(this["explorerSmartMode"]));
+            }
+            set {
+                this["explorerSmartMode"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool conversionDisjoint {
+            get {
+                return ((bool)(this["conversionDisjoint"]));
+            }
+            set {
+                this["conversionDisjoint"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool fallbackSearchEnabled {
+            get {
+                return ((bool)(this["fallbackSearchEnabled"]));
+            }
+            set {
+                this["fallbackSearchEnabled"] = value;
             }
         }
     }
